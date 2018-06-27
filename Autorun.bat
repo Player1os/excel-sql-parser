d853a96682bef9f1696f51d9a59a6222e84cd9b0
Osama Hassanein <player1os@gmail.com>
2018-06-27 16:52:37 +0200

    Simplify the autorun script

--- Autorun.bat
+++ Autorun.bat
@@ -1,8 +1,5 @@
 @ECHO OFF
 
-:: Switch to the deploy directory.
-PUSHD \\kedata\Data1\B2B_Business_Inteligence\Osama Hassanein\ExcelSQLParser
-
 :: Set the autorun parameters.
 SET APP_IS_AUTORUN_MODE=TRUE
 SET APP_INPUT_WORKBOOK_FILE_PATH=H:\INPUT_WORKBOOK.xlsx
@@ -11,6 +8,3 @@ SET APP_OUTPUT_WORKBOOK_FILE_PATH=H:\OUTPUT_WORKBOOK.xlsx
 
 :: Run the main project workbook.
 CALL "SQLParser.xlsm"
-
-:: Return to the original working directory.
-POPD
